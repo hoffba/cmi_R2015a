98353f03e37a323068513a06fb9e3a3a53732e31
hoffba <hoffba@gmail.com>
2015-05-06 09:16:02 -0400

    adjustGap fillval

--- adjustGap.m
+++ adjustGap.m
@@ -1,18 +1,20 @@
 % Script to adjust gapped data, inserting blank slices
-function adjustGap(cmiObj,targetdz)
+function adjustGap(cmiObj,targetdz,fillval)
 
-ovoxsz = cmiObj.img.voxsz;
-dnew = floor(ovoxsz(3)/targetdz);
-% vnew = ovoxsz(3)/dnew;
+if nargin<3
+    fillval = 0;
+end
+
+dnew = floor(cmiObj.img.voxsz(3)/targetdz);
 d = cmiObj.img.dims;
-fov = d(1:3).*ovoxsz;
+fov = d(1:3).*cmiObj.img.voxsz;
 d(3) = dnew * d(3);
 ind = dnew/2:dnew:d(3);
 
 omask = cmiObj.img.mask.mat;
 
 % Set new image:
-iimg = zeros(d)-2000;
+iimg = zeros(d)+fillval;
 iimg(:,:,ind,:) = cmiObj.img.mat;
 cmiObj.setImg(iimg,cmiObj.img.labels,fov);
 
