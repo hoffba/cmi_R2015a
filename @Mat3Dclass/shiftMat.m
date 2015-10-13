05c4772b23b6570742dda6c2c08f4b2296a2a930
hoffba <hoffba@gmail.com>
2015-10-13 16:42:07 -0400

    fixed shift

--- @Mat3Dclass/shiftMat.m
+++ @Mat3Dclass/shiftMat.m
@@ -1,7 +1,13 @@
-% ImageClass function
+% Mat3Dclass function
 function shiftMat(self,v,dx)
-% Manually shift the matrix
+% Shift (circular) the matrix
 
 if self.check && all(ismember(v,1:self.dims(4))) && (length(dx)==3)
+    
+    % Shift the image matrix
     self.mat(:,:,:,v) = circshift(self.mat(:,:,:,v),round(dx));
+    
+    if isprop(self,'mask')
+        self.mask.shiftMat(1,dx);
+    end
 end
