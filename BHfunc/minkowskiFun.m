3989ea7ea3cec8e6c48ad67491712553626e72bf
hoffba <hoffba@users.noreply.github.com>
2020-02-25 11:03:44 -0500

    update to use imVolume instead of imVolumeEstimate

--- BHfunc/minkowskiFun.m
+++ BHfunc/minkowskiFun.m
@@ -170,7 +170,7 @@ end
 function p = calcMF3D(BW,voxsz,ord)
 p = nan(1,length(ord));
 if ismember(0,ord)
-    p(ord==0) = imVolumeEstimate(BW,voxsz);
+    p(ord==0) = imVolume(BW,voxsz);
 end
 if ismember(1,ord)
     p(ord==1) = imSurfaceEstimate(BW,voxsz);
