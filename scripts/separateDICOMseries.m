314d34105e7686d78a4816b35b22fdd3c68395ce
hoffba <hoffba@users.noreply.github.com>
2021-08-10 13:59:20 -0400

    Update separateDICOMseries.m

--- scripts/separateDICOMseries.m
+++ scripts/separateDICOMseries.m
@@ -7,22 +7,48 @@ if nargin==0
     bpath = pwd;
 end
 
-dnames = dir(bpath);
-dnames = {dnames(cellfun(@isfolder,{dnames.name})).name}';
-dnames(1:2) = [];
+% Find subdirectories to the base path
+n = length(bpath)+1;
+dnames = strsplit(genpath(bpath),';')';
+dnames = cellfun(@(x)x(n+1:end),dnames,'UniformOutput',false);
+dnames(cellfun(@isempty,dnames)) = [];
 answer = listdlg('ListString',dnames);
 dnames = dnames(answer);
+
+% Perform separation on files in each folder:
 for idir = 1:length(dnames)
 
     tpath = fullfile(bpath,dnames{idir});
+    
+    % Find DICOM files:
     fnames = dir(fullfile(tpath,'*.dcm'));
-    for i = 1:length(fnames)
-        info = dicominfo(fullfile(tpath,fnames(i).name));
-        svdir = info.SeriesNumber;
-        if ~isfolder(fullfile(tpath,svdir))
-            mkdir(fullfile(tpath,svdir));
+    if isempty(fnames)
+        % Check for files with no extension
+        fnames = dir(tpath);
+        fnames(contains({fnames.name},'.')) = [];
+        fnames([fnames.isdir]) = [];
+        if isempty(fnames)
+            fprintf('No DICOM files found in directory: %s\n',dnames{idir});
         end
-        movefile(fullfile(tpath,fnames(i).name),fullfile(tpath,svdir));
+    end
+    
+    % Move DICOM files to new subfolders
+    for i = 1:length(fnames)
+        shortname = fullfile(dnames{idir},fnames(i).name);
+        if ~strcmp(fnames(i).name,'DICOMDIR')
+            fullname = fullfile(tpath,fnames(i).name);
+            if isdicom(fullname)
+                info = dicominfo(fullname);
+                svdir = info.SeriesNumber;
+                if ~isfolder(fullfile(tpath,svdir))
+                    mkdir(fullfile(tpath,svdir));
+                end
+                fprintf('Moving file: %s\n',shortname);
+                movefile(fullname,fullfile(tpath,svdir));
+            else
+                fprintf('Not a DICOM: %s\n',shortname);
+            end
+        end
     end
     
 end
