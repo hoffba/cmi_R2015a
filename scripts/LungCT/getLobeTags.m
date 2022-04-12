191a3eaa98f1155c54a164a0feda18b1853452d8
hoffba <hoffba@users.noreply.github.com>
2022-04-12 12:56:16 -0400

    Update getLobeTags.m

--- scripts/LungCT/getLobeTags.m
+++ scripts/LungCT/getLobeTags.m
@@ -1,21 +1,22 @@
 % Determine lobe tags for various lung segmentations
 function lobe = getLobeTags(seg)
     utags = unique(seg(seg>0));
-    if all(ismember(10:10:60,utags))
+    
+    if all(ismember(utags,[10,20]))
+        lobeTag = [10 20];
+        lobeName = {'RL', 'LL'};
+    elseif all(ismember(utags,10:10:60))
         % YACTA?
         lobeTag = 10:10:60;
         lobeName = {'RUL','RML','RLL','LUL','LLi','LLL'};
-    elseif all(ismember(1:5,utags))
+    elseif all(ismember(utags,1:5))
         % YACTA
         lobeTag = 1:5;
         lobeName = {'LUL','LLL','RUL','RLL','RML'};
-    elseif all(ismember([11,12,13,21,22],utags))
+    elseif all(ismember(utags,[11,12,13,21,22]))
         % ImBio
         lobeTag = [11,12,13,21,22];
         lobeName = {'RUL','RLL','RML','LUL','LLL'};
-    elseif all(ismember(10:10:20,utags))
-        lobeTag = [10 20];
-        lobeName = {'RL', 'LL'};
     else
         error('Could not match valid segmentation labeling schema: %s',num2str(utags'));
     end
