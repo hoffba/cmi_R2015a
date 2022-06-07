be5901de211032fa39716ee6bdf3738c9d3dd7e4
hoffba <hoffba@users.noreply.github.com>
2022-06-07 15:52:53 -0400

    Update CTlung_Segmentation.m

--- scripts/LungCT/CTlung_Segmentation.m
+++ scripts/LungCT/CTlung_Segmentation.m
@@ -17,43 +17,41 @@ if method==3 && ~gpuDeviceCount
     method = 2;
 end
 
-for i = 1:2
-    switch method
-        case 1
-            writeLog(logfn,'- Generating VOI from Step02_segLungHuman_cjg ...\n');
-            seg = segLungHuman_cjg_bh(1,ct);
-        case 2
-            writeLog(logfn,'- Generating VOI from getRespiratoryOrgans ...\n');
-            seg = getRespiratoryOrgans(ct);
-        case 3
-            fprintf(logfn,'- Generating VOI from DL_lung_segmetation ...\n');
-            seg = DL_lung_segmetation(ct);
-        case 4
-            writeLog(logfn,'- Generating VOI from YACTA ...\n');
-            ydir = fullfile(savepath,['yacta_',id]);
-            if ~isfolder(ydir)
-                mkdir(ydir);
+switch method
+    case 1
+        writeLog(logfn,'- Generating VOI from Step02_segLungHuman_cjg ...\n');
+        seg = segLungHuman_cjg_bh(1,ct);
+    case 2
+        writeLog(logfn,'- Generating VOI from getRespiratoryOrgans ...\n');
+        seg = getRespiratoryOrgans(ct);
+    case 3
+        fprintf(logfn,'- Generating VOI from DL_lung_segmetation ...\n');
+        seg = DL_lung_segmetation(ct);
+    case 4
+        writeLog(logfn,'- Generating VOI from YACTA ...\n');
+        ydir = fullfile(savepath,['yacta_',id]);
+        if ~isfolder(ydir)
+            mkdir(ydir);
+        end
+        tname = fullfile(ydir,sprintf('%s.mhd',id));
+        saveMHD(tname,ct,id,info.fov,info.orient);
+        yacta(tname,'wait');
+        resname = dir([tname,'*lung_lobes*explabels.mhd']);
+        if ~isempty(resname)
+            seg = cmi_load(1,[],fullfile(ydir,resname(end).name));
+        end
+        if ~any(seg,'all')
+            writeLog(logfn,'- - Lobe segmentation failed. Checking for lungs...');
+            resname = dir([tname,'*lung_right_left*explabels.mhd']);
+            if ~isempty(resname)
+                seg = cmi_load(1,[],fullfile(ydir,resname(end).name));
             end
-            tname = fullfile(ydir,sprintf('%s.mhd',id));
-            saveMHD(tname,ct,id,info.fov,info.orient);
-            yacta(tname,'wait');
-            tname = dir([tname,'*lung_lobes*explabels.mhd']);
-            if ~isempty(tname)
-                seg = cmi_load(1,[],fullfile(ydir,tname(end).name));
+            if any(seg,'all')
+                writeLog(logfn,' Using R/L segmentation\n');
+            else
+                writeLog(logfn,' FAILED\n');
             end
-            if ~any(seg,'all')
-                writeLog(logfn,'- - Lobe segmentation failed. Checking for lungs...');
-                tname = dir([tname,'*.mhd']);
-                if ~isempty(tname)
-                    seg = cmi_load(1,[],fullfile(ydir,tname(end).name));
-                end
-                if any(seg,'all')
-                    writeLog(logfn,' Using R/L segmentation\n');
-                else
-                    writeLog(logfn,' FAILED\n');
-                end
-            end
-        otherwise
-    end
+        end
+    otherwise
 end
 
