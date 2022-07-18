09a429de4b8a8452625a1eaca61528a8714d896c
Craig Galban <cgalban@umich.edu>
2022-07-18 13:27:20 -0400

    Series and Study UID
    had to pull UIDs out of loop to get files to associate with single series. I think it works.

--- save/saveDICOM.m
+++ save/saveDICOM.m
@@ -1,74 +1,87 @@
-function status = saveDICOM(fname,img,label,fov,info,verbchk)
+function [status info] = saveDICOM(fname,img,label,fov,info,verbchk)
 status = false;
-if (nargin>=4)
-    if nargin<6
-        verbchk = true;
+
+if nargin<1
+    [FileName,PathName,FilterIndex] = uigetfile('*.*','Select data File for Conversion to Dicom.');
+    img = niftiread(fullfile(PathName,FileName));
+    info = niftiinfo(fullfile(PathName,FileName));
+    label = 'temp';
+    fov = size(img) .* info.PixelDimensions;
+end
+
+% if (nargin>=4)
+if nargin<6
+    verbchk = true;
+end
+
+[d(2),d(1),ns,nv] = size(img);
+% First check that the file name is correct
+[pathstr, fname, ~] = fileparts(FileName);
+if ~isempty(label) && ~isnumeric(label)
+    if ischar(label)
+        label = {label};
     end
-    
-    [d(2),d(1),ns,nv] = size(img);
-    % First check that the file name is correct
-    [pathstr, fname, ~] = fileparts(fname);
-    if ~isempty(label) && ~isnumeric(label)
-        if ischar(label)
-            label = {label};
-        end
-        if ~iscellstr(label) || (length(label)~=nv)
-            label = strcat(label(1),num2cell(num2str((1:nv)')));
-        end
-    end
-    
-    % Auto-scale to int16:
-    mxscale = 2^15-1;
-    imax = squeeze(max(max(max(img,[],1),[],2),[],3));
-    imin = squeeze(min(min(min(img,[],1),[],2),[],3));
-    sthk = fov(3)/ns;
-    spos = (sthk - fov(3))/2;
-    warning('off','images:dicomwrite:inconsistentIODAndCreateModeOptions');
-    
-    % Determine DICOM header tags:
-    if (nargin<5)
-        info = {};
-    end
-    info = parseinfo(info);
-    info.PixelSpacing = fov(1:2)./d;
-    info.SpacingBetweenSlices = sthk;
-    info.SliceThickness = sthk;
-    
-    for ivec = 1:nv
-        info.AcquisitionNumber = ivec;
-        info.RescaleIntercept = imin(ivec);
-        info.RescaleSlope = (imax(ivec) - imin(ivec))/mxscale;
-        if info.RescaleSlope == 0
-            info.RescaleSlope = 1;
-        end
-        info.SeriesDescription = label{ivec};
-        info.ImagePositionPatient = [0,0,spos];
-        
-        if nv>1
-            fnout = [fname,'_',num2str(ivec)];
-        else
-            fnout = fname;
-        end
-        
-        if ~exist(fullfile(pathstr,fnout),'dir')
-            mkdir(fullfile(pathstr,fnout));
-        end
-        if verbchk, hw = waitbar(0,'Saving DICOM slices ...','Name','Saving DICOM image ...'); end
-        for islc = 1:ns
-            info.ImagePositionPatient(3) = info.ImagePositionPatient(3) + sthk;
-            info.SliceLocation = info.ImagePositionPatient(3);
-            info.InstanceNumber = islc;
-            timg = int16((img(:,:,islc,ivec)-info.RescaleIntercept)/info.RescaleSlope);
-            stat = dicomwrite(timg,fullfile(pathstr,fnout,sprintf([fnout,'_s%04u.dcm'],islc)),info);
-            status = status && isempty(stat.BadAttribute) ...
-                && isempty(stat.MissingCondition) ...
-                && isempty(stat.MissingData) ...
-                && isempty(stat.SuspectAttribute);
-            if verbchk, waitbar(islc/ns,hw,['Slc ',num2str(info.ImagePositionPatient(3))]); end
-        end
-        if verbchk, close(hw); end
+    if ~iscellstr(label) || (length(label)~=nv)
+        label = strcat(label(1),num2cell(num2str((1:nv)')));
     end
 end
+
+% Auto-scale to int16:
+mxscale = 2^15-1;
+imax = squeeze(max(max(max(img,[],1),[],2),[],3));
+imin = squeeze(min(min(min(img,[],1),[],2),[],3));
+sthk = fov(3)/ns;
+spos = (sthk - fov(3))/2;
+warning('off','images:dicomwrite:inconsistentIODAndCreateModeOptions');
+
+% Determine DICOM header tags:
+if (nargin<5)
+    info = [];
+else
+    info = parseinfo(info);
+end
+info.PixelSpacing = fov(1:2)./d;
+info.SpacingBetweenSlices = sthk;
+info.SliceThickness = sthk;
+info.SeriesInstanceUID = dicomuid;
+info.StudyInstanceUID = dicomuid;
+
+for ivec = 1:nv
+    info.AcquisitionNumber = ivec;
+    info.RescaleIntercept = imin(ivec);
+    info.RescaleSlope = (imax(ivec) - imin(ivec))/mxscale;
+    if info.RescaleSlope == 0
+        info.RescaleSlope = 1;
+    end
+    info.SeriesDescription = label{ivec};
+    info.ImagePositionPatient = [0,0,spos];
+
+    if nv>1
+        fnout = [fname,'_',num2str(ivec)];
+    else
+        fnout = fname;
+    end
+
+    if ~exist(fullfile(pathstr,fnout),'dir')
+        mkdir(fullfile(pathstr,fnout));
+    end
+    if verbchk, hw = waitbar(0,'Saving DICOM slices ...','Name','Saving DICOM image ...'); end
+
+    for islc = 1:ns
+        info.ImagePositionPatient(3) = info.ImagePositionPatient(3) + sthk;
+        info.SliceLocation = info.ImagePositionPatient(3);
+        info.InstanceNumber = islc;
+        timg = int16((img(:,:,islc,ivec)-info.RescaleIntercept)/info.RescaleSlope);
+        stat = dicomwrite(timg,fullfile(pathstr,fnout,sprintf([fnout,'_s%04u.dcm'],islc)),info);
+        status = status && isempty(stat.BadAttribute) ...
+            && isempty(stat.MissingCondition) ...
+            && isempty(stat.MissingData) ...
+            && isempty(stat.SuspectAttribute);
+        if verbchk, waitbar(islc/ns,hw,['Slc ',num2str(info.ImagePositionPatient(3))]); end
+    end
+    if verbchk, close(hw); end
+end
+% end
 
 function info = parseinfo(info)
 info = [fieldnames(info)';struct2cell(info)'];
@@ -93,8 +106,8 @@ addParameter(p,'SpacingBetweenSlices',1,@isvector);
 addParameter(p,'SliceThickness',1,@isvector);
 addParameter(p,'RescaleSlope',1,@isvector);
 addParameter(p,'RescaleIntercept',0,@isvector);
-addParameter(p,'SeriesInstanceUID',dicomuid,@ischar);
-addParameter(p,'StudyInstanceUID',dicomuid,@ischar);
+% addParameter(p,'SeriesInstanceUID',dicomuid,@ischar);
+% addParameter(p,'StudyInstanceUID',dicomuid,@ischar);
 parse(p,info{:});
 info = p.Results;
 info.PatientName = struct('FamilyName',{''},'GivenName',info.PatientID,...
