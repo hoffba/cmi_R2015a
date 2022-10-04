db40317a07357f124bbba422b22059fc9c0acf8b
hoffba <hoffba@users.noreply.github.com>
2022-10-04 14:23:36 -0400

    Update Main_Pipeline_select.m

--- scripts/LungCT/Main_Pipeline_select.m
+++ scripts/LungCT/Main_Pipeline_select.m
@@ -13,7 +13,10 @@ function j = Main_Pipeline_select(varargin)
 % Main_Pipeline_select( dcm_path, save_path, 'quickreg', 'serialbatch' )
 
 cluster_profile = {};
+
+% Processing flag: True means run serially on command window
 flag = false;
+
 opts = struct('dcm_path','',...
               'save_path','',...
               'quickreg',false);
@@ -27,18 +30,19 @@ if nargin
             opts.quickreg = ismember('quickreg',varargin);
             if ismember('serial',varargin)
                 flag = true;
-            elseif ismember('serialbatch',varargin)
-                cluster_profile = {'Profile','cmi_1'};
-                if ~ismember('cmi_1',parallel.clusterProfiles)
-                    mls_fname = fullfile(fileparts(which('cmi')),'cmi_1.mlsettings');
-                    if exist(mls_fname,'file')
-                        fprintf('Importing cluster profile: %s\n',mls_fname);
-                        parallel.importProfile(mls_fname);
-                    else
-                        fprintf('Could not find cluster profile: %s\n',mls_fname);
-                        cluster_profile = {};
-                    end
-                end
+%             elseif ismember('serialbatch',varargin)
+%                 flag = 2;
+%                 cluster_profile = {'Profile','cmi_1'};
+%                 if ~ismember('cmi_1',parallel.clusterProfiles)
+%                     mls_fname = fullfile(fileparts(which('cmi')),'cmi_1.mlsettings');
+%                     if exist(mls_fname,'file')
+%                         fprintf('Importing cluster profile: %s\n',mls_fname);
+%                         parallel.importProfile(mls_fname);
+%                     else
+%                         fprintf('Could not find cluster profile: %s\n',mls_fname);
+%                         cluster_profile = {};
+%                     end
+%                 end
             end
         end
     end
@@ -47,25 +51,28 @@ end
 %% Find files
 [cases,opts] = catalog_select_3('opts',opts);
 
-if ~any(ismember({'serial','serialbatch'},varargin))
-    parpool('local',opts.par_size);
-end
-
 %% Loop over cases
 ncases = numel(cases);
 for i = 1:ncases
     
     basename = sprintf('%s_%s',cases(i).UMlabel,cases(i).StudyDate);
     procdir = fullfile(opts.save_path,basename);
-    
-    if flag
+    if flag % serial
         % Start pipeline in command window
         fprintf('%s - starting Main_Pipeline_sub case #%u\n',basename,i);
         Main_Pipeline_sub(basename,cases(i).Scans.Directory,procdir,opts);
     else
-        % Start pipeline batch job
-        fprintf('%s - starting Main_Pipeline_sub as batch job: #',basename);
-        j(i) = batch(@Main_Pipeline_sub,1,[{basename},{cases(i).Scans.Directory},{procdir},{opts}],cluster_profile{:});
-        fprintf('%u\n',j(i).ID);
+        if i==1
+            myCluster = parcluster;
+            myCluster.NumWorkers = opts.par_size;
+        end
+        fprintf('%s - starting Main_Pipeline_sub case #%d\n',basename,i);
+        j(i) = batch(myCluster,@Main_Pipeline_sub,1,[{basename},{cases(i).Scans.Directory},{procdir},{opts}]);
     end
+%         case 2 % serialbatch
+%             % Start pipeline batch job
+%             fprintf('%s - starting Main_Pipeline_sub as batch job: #',basename);
+%             j(i) = batch(@Main_Pipeline_sub,1,[{basename},{cases(i).Scans.Directory},{procdir},{opts}],cluster_profile{:});
+%             fprintf('%u\n',j(i).ID);
+%     end
 end
