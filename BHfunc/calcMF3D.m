2087430c1d55d671e34b9bbff7ceea399c76d890
hoffba <hoffba@users.noreply.github.com>
2024-02-26 09:22:02 -0500

    Update calcMF3D.m

--- BHfunc/calcMF3D.m
+++ BHfunc/calcMF3D.m
@@ -1,20 +1,22 @@
 % Raw 3D MF calculation
 function p = calcMF3D(BW,voxsz,ord,mask)
 if nargin==4
-    normval = imVolumeEstimate(mask,voxsz);
+    normvol = imVolumeEstimate(mask,voxsz);
+    normN = nnz(mask);
 else
-    normval = 1;
+    normvol = 1;
+    normN = 1;
 end
 p = nan(1,length(ord));
 if ismember(0,ord)
-    p(ord==0) = imVolumeEstimate(BW,voxsz)/normval;
+    p(ord==0) = imVolumeEstimate(BW,voxsz)/normvol;
 end
 if ismember(1,ord)
-    p(ord==1) = imSurfaceEstimate(BW,voxsz)/normval;
+    p(ord==1) = imSurfaceEstimate(BW,voxsz)/normvol;
 end
 if ismember(2,ord)
-    p(ord==2) = imMeanBreadth(BW,voxsz)/normval;
+    p(ord==2) = imMeanBreadth(BW,voxsz)/normvol;
 end
 if ismember(3,ord)
-    p(ord==3) = imEuler3dEstimate(BW);
+    p(ord==3) = imEuler3dEstimate(BW)/normN;
 end
